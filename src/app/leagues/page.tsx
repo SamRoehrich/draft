d7869a5980ba94b933ecb28a7813670824694646
SamRoehrich <sam_roehrich@icloud.com>
2024-03-24 22:14:22 -0700

    chore: add metadata

--- src/app/leagues/page.tsx
+++ src/app/leagues/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "League Page: Lifetime Grand Prix draft",
+};
+
 export default function LeaguePage() {
   return (
     <main className="flex min-h-screen flex-col">
